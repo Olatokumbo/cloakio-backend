b85385dc341f5f6c8f7c504da0acc43379fae9b5
David Faith <faithodesola@gmail.com>
2022-01-01 16:07:10 +0000

    Integrating firebase

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import express, { Response, Request } from "express";
 import mongooseConnectionDB from "./config/database";
-require('dotenv').config();
+// import admin from "./config/firebase";
+import auth from "./middlewares/auth";
+require("dotenv").config();
 
 const PORT: number | string = process.env.PORT || 8000;
 const app = express();
 
 // Connecting to MongoDB
 mongooseConnectionDB(process.env.MONGODB_URI!);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (_req: Request, res: Response) => {
+app.get("/", auth, (_req: Request, res: Response) => {
   res.status(200).json({
     response: "Hello from Cloakio's Official Server",
   });
---
+++ src/middlewares/auth.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import admin from "../config/firebase";
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+  const headerToken = req.headers.authorization;
+  if (!headerToken) {
+    return res.send({ message: "No token provided" }).status(401);
+  }
+  if (headerToken && headerToken.split(" ")[0] !== "Bearer") {
+    res.status(401).json({ message: "Invalid token" });
+  }
+
+  const token = headerToken.split(" ")[1];
+  admin
+    .auth()
+    .verifyIdToken(token)
+    .then(() => next())
+    .catch(() => res.status(403).json({ message: "Could not authorize" }));
+};
+
+export default auth;
